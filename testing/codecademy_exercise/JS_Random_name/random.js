c417634579141288e74ef166cdbca87a7df3cbda
Kenson999 <86390435+Kenson999@users.noreply.github.com>
2021-08-04 17:36:24 +0800

    Update random.js

--- testing/codecademy_exercise/JS_Random_name/random.js
+++ testing/codecademy_exercise/JS_Random_name/random.js
@@ -17,11 +17,14 @@ const geT = () => {
 const go = geT();
 console.log(go)
 
-
+///////////////////////////////////////////////
 function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  getRandomNum(5, 10) // 隨機回傳 5 - 10 之間的整數
-  let arr = ['Dylan', 'Jack', 'Ralph', 'Tom', 'Rex']
-  console.log(arr[getRandomNum(0, arr.length - 1)]) // 回傳 arr 中隨機成員
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomNum(5, 10) // 隨機回傳 5 - 10 之間的整數
+let arr = ['1', '2', '3', '4','5']
+console.log(arr[getRandomNum(0, arr.length - 1)]) // 回傳 arr 中隨機成員
+///////////////////////////////////////////////
+
+let arr2 = ['A', 'B', 'C', 'D', 'E']
+console.log(arr2[Math.floor(Math.random()*arr2.length)])
